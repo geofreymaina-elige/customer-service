fa4c6758a6c270f2524b2dccf3eaddb731af4a02
mainageofrey731@gmail.com <Jeff>
2026-09-03 17:03:25 +0300

    chore: use ASTPP_TOKEN_KEY_HEX and ASTPP_IV_HEX exclusively

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -27,8 +27,8 @@ export default () => ({
     database: process.env.ASTPP_DATABASE || 'astpp',
     user: process.env.ASTPP_USER || 'root',
     password: process.env.ASTPP_PASSWORD || '',
-    tokenKey: process.env.ASTPP_TOKEN_KEY || '',
-    ivKey: process.env.ASTPP_IV_KEY || '',
+    tokenKeyHex: process.env.ASTPP_TOKEN_KEY_HEX || '',
+    ivHex: process.env.ASTPP_IV_HEX || '',
   },
   kafka: {
     brokers: (process.env.KAFKA_BROKERS || 'localhost:9092').split(','),
